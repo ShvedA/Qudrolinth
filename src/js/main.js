1459b1345ffb7e7500ecde8646fc7bdb9bd56dd5
aleksandr.sved <aleksandr.sved@helmes.ee>
2014-11-10 16:40:03 +0200

    Labirinth representation taken from javascript arrays

---
+++ src/js/main.js
@@ -0,0 +1,54 @@
+var labirinth = new Array();
+var sides = new Array("Border-Right", "Border-Bottom");
+for (var rows = 0; rows < 9; rows++) 
+labirinth[rows] = new Array();
+labirinth[0][0] = new Array(0,0,0,0,0,0,0,0,0);
+labirinth[0][1] = new Array(0,1,1,1,1,1,1,1,1);
+labirinth[1][0] = new Array(1,0,1,1,1,0,0,0,1);
+labirinth[1][1] = new Array(0,0,0,0,0,0,1,1,0);
+labirinth[2][0] = new Array(1,1,0,1,0,0,0,1,1);
+labirinth[2][1] = new Array(0,0,1,0,0,0,0,0,0);
+labirinth[3][0] = new Array(1,1,0,0,1,1,1,1,1);
+labirinth[3][1] = new Array(0,0,0,1,1,0,0,0,0);
+labirinth[4][0] = new Array(1,1,1,0,0,0,1,0,1);
+labirinth[4][1] = new Array(0,0,0,0,0,0,1,1,1);
+labirinth[5][0] = new Array(1,0,0,1,1,1,0,0,1);
+labirinth[5][1] = new Array(0,1,1,0,0,0,0,0,0);
+labirinth[6][0] = new Array(1,0,0,1,1,0,0,1,1);
+labirinth[6][1] = new Array(0,0,0,0,0,1,1,0,0);
+labirinth[7][0] = new Array(1,1,1,0,0,0,0,1,1);
+labirinth[7][1] = new Array(0,0,1,1,0,0,1,0,0);
+labirinth[8][0] = new Array(1,0,0,0,1,0,0,0,1);
+labirinth[8][1] = new Array(0,1,1,1,1,1,1,1,1);
+
+function showLabirinth() {
+    var div = document.getElementById("secondTable");
+    var table = document.createElement("table");
+    table.className = "table table-bordered table-board";
+    var tbody = document.createElement("tbody");
+    var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    for(var row = 0; row < labirinth.length; row++) {
+        var tr = document.createElement("tr");
+        for (var col = 0; col < labirinth[row][0].length; col++) {
+            var td = document.createElement("td")
+            if (row == 0 && col > 0) {
+                td.innerHTML=alphabet.charAt(col-1);   
+            }
+            if (col == 0 && row > 0) {
+                td.innerHTML=row;   
+            }
+            if (labirinth[row][0][col] == 1) {
+                td.className += " right-border";
+                //document.getElementById("secondTable").write("right-border");
+            }
+            if (labirinth[row][1][col] == 1) {
+                td.className += " bottom-border";
+                //document.getElementById("secondTable").write(" bottom-border");   
+            }
+            tr.appendChild(td);
+        }
+        tbody.appendChild(tr);
+    }
+    table.appendChild(tbody);
+    div.appendChild(table);
+}
